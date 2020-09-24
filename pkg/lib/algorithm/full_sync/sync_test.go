fa13f18394a946e5d61d06740291f6a242582ae6
bowenislandsong <bowenislandsong@gmail.com>
2020-09-24 12:59:06 -0400

    (fix) Add error msg to GetAddtionals if last sync failed

--- pkg/lib/algorithm/full_sync/sync_test.go
+++ pkg/lib/algorithm/full_sync/sync_test.go
@@ -78,9 +78,12 @@ func TestNewFullSetSync(t *testing.T) {
 		err = server.SyncClient("", 8080)
 		assert.NoError(t, err)
 		wg.Wait()
-
-		assert.Len(t,*client.GetSetAdditions(),tt.serverSetSize-tt.intersectionSize)
-		assert.Len(t,*server.GetSetAdditions(),tt.clientSetSize-tt.intersectionSize)
+		cadd, err := client.GetSetAdditions()
+		assert.NoError(t, err)
+		sadd, err := server.GetSetAdditions()
+		assert.NoError(t, err)
+		assert.Len(t, *cadd, tt.serverSetSize-tt.intersectionSize)
+		assert.Len(t, *sadd, tt.clientSetSize-tt.intersectionSize)
 		assert.EqualValues(t, *server.GetLocalSet(), *client.GetLocalSet())
 	}
 }
